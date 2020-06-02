45093a990b94177aa6ec47b46cb57d9c61e0cc5c
Elías Huerta <elias.huerta@icloud.com>
2020-06-02 17:11:55 +0200

    Routes for navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ChooseFestivalComponent } from './choose-festival/choose-festival.component';
+import { ChooseArtistComponent } from './choose-artist/choose-artist.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: "full", redirectTo: "/login" },
+  { path: "login", component: LoginComponent },
+  { path: "choose-fest", component: ChooseFestivalComponent },
+  { path: "choose-artist", component: ChooseArtistComponent },
+  { path: "**", redirectTo: "/login" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
