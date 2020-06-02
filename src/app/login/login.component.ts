4b59539f24e50ece35fa706043e1ab7d16afcaf1
Elías Huerta <elias.huerta@icloud.com>
2020-06-02 18:09:12 +0200

    Form login and register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  formUser: FormGroup;
+  formRegister: FormGroup;
+  constructor() {
+    this.formUser = new FormGroup({
+      username: new FormControl('', [
+        Validators.pattern(/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/)
+      ]),
+      pass: new FormControl('', [
+        Validators.required
+      ])
+    });
+    this.formRegister = new FormGroup({
+      username: new FormControl('', [
+        Validators.pattern(/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/)
+      ]),
+      pass: new FormControl('', [
+        Validators.required
+      ])
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onClick() {
+    console.log(this.formUser.value);
+  }
+
+  register() {
+    console.log(this.formRegister.value)
   }
 
 }
