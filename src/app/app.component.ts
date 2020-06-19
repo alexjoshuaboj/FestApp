4df33294e1bde9d0c70e4038c1325d46e3c41c6b
Elías Huerta <elias.huerta@icloud.com>
2020-06-19 09:58:39 +0200

    Nav hidden in login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +8,11 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
 
-  constructor(public route: Router) {
+  constructor(public route: Router, public activatedRoute: ActivatedRoute) {
 
   };
 
   ngOnInit() {
-
   }
 
 
