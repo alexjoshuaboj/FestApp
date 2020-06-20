88a1cbf3cae0b76f43e76334b8f0bf2851a0512f
alexjoshua <alejandrobojneag@gmail.com>
2020-06-20 11:05:47 +0200

    spotify and facebook buttons READY; :boom:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { ChooseFestivalComponent } from './choose-festival/choose-festival.component';
 import { ChooseArtistComponent } from './choose-artist/choose-artist.component';
 import { LoginGuardGuard } from './login-guard.guard';
+import { UserComponent } from './user/user.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: "full", redirectTo: "/login" },
   { path: "login", component: LoginComponent },
+  { path: "users", component: UserComponent },
   { path: "choose-fest", component: ChooseFestivalComponent, canActivate: [LoginGuardGuard] },
   { path: "choose-artist", component: ChooseArtistComponent, canActivate: [LoginGuardGuard] },
   { path: "**", redirectTo: "/login" }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  notes: string[] = ['Hola', 'Clogs', 'Beunas', 'Moccasins', 'Sneakers'];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getUserSpotify() {
+
   }
 
 }
